f6af36b60611649c8a66bd2ac1eead89381485a2
personball <personball@163.com>
2019-01-19 15:46:59 +0800

    rename project

--- angular-ngalain/e2e/src/app.e2e-spec.ts
+++ angular-ngalain/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-ngalain!');
+    expect(page.getTitleText()).toEqual('Welcome to AbpProjectName!');
   });
 });
