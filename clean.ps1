2b3d02e7e287aeb9ca447f0a4710adb1e66cdd9d
personball <personball@163.com>
2019-01-28 13:54:17 +0800

    use dotnet clean

--- clean.ps1
+++ clean.ps1
@@ -1,18 +1,8 @@
 $paths=@( 
 ".\vue\dist\",
 ".\vue\node_modules\",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Application\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Application\obj",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Core\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Core\obj",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.EnityFrameworkCore\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.EnityFrameworkCore\obj",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Migrator\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Migrator\obj",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Web.Core\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Web.Core\obj",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Web.Host\bin",
-".\aspnet-core\src\AbpCompanyName.AbpProjectName.Web.Host\obj"
+".\angular\node_modules\",
+".\angular-ngalain\node_modules\"
 )
 
 function Clean{
@@ -28,3 +18,4 @@ function Clean{
 
 foreach($path in $paths){Clean($path)}
 
+dotnet clean -v n .\aspnet-core\AbpCompanyName.AbpProjectName.sln
