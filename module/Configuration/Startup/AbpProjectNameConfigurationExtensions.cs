0eca0a41d7e4dc4881dfffa8825b9de118930fa0
personball <personball@163.com>
2019-02-28 16:09:30 +0800

    add startup  configuration for module

---
+++ module/Configuration/Startup/AbpProjectNameConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Configuration.Startup;
+
+namespace AbpCompanyName.AbpProjectName.Configuration.Startup
+{
+    public static  class AbpModuleNameConfigurationExtensions
+    {
+        public static IAbpModuleNameModuleConfiguration UseAbpModuleName(this IModuleConfigurations configurations)
+        {
+            return configurations.AbpConfiguration.GetOrCreate("Modules.AbpCompanyName.AbpProjectName.AbpModuleName", 
+                () => configurations.AbpConfiguration.IocManager.Resolve<IAbpModuleNameModuleConfiguration>());
+        }
+    }
+}
