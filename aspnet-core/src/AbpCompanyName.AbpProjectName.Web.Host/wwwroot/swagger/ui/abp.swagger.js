7f58d20f18f8a563ff99db785857ebbf37eac952
personball <personball@outlook.com>
2019-12-15 17:26:57 +0800

    Update abp.swagger.js
    修复 swagger 界面登陆机制未提交租户标识

--- aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/wwwroot/swagger/ui/abp.swagger.js
+++ aspnet-core/src/AbpCompanyName.AbpProjectName.Web.Host/wwwroot/swagger/ui/abp.swagger.js
@@ -58,7 +58,8 @@ var abp = abp || {};
         xhr.open('POST', '/api/TokenAuth/Authenticate', true);
         xhr.setRequestHeader('Abp.TenantId', tenantId);
         xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.send("{" + "usernameOrEmailAddress:'" + usernameOrEmailAddress + "'," + "password:'" + password + "'}");
+        var tenancyName = document.getElementById('tenancyName').value;
+        xhr.send("{" + "tenancyName:'" + tenancyName + "'," + "usernameOrEmailAddress:'" + usernameOrEmailAddress + "'," + "password:'" + password + "'}");
     };
 
     abp.swagger.login = function (callback) {
